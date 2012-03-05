19dcd9689fb7439bc1cc56810aa11f596a233ce8
João Soares <jsoaresgeral@gmail.com>
2012-03-05 16:45:07 +0000

    added some symbols tests

--- test/test_symbols.rb
+++ test/test_symbols.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require 'benchmark'
 
 class TestSymbols < Test::Unit::TestCase
 
@@ -12,6 +13,25 @@ class TestSymbols < Test::Unit::TestCase
     assert_equal %s["], :'"', "Should return true since both represent the weird symbol :\""
   end
 
-  
+  def test_convertion_symbol_to_string
+    sym = :string
+    assert_equal "string", sym.to_s, "Expected to convert symbol to string"
+    assert_equal "string", sym.id2name, "Expected to convert symbol to string"
+  end
 
+  def test_convertion_string_to_symbol
+    str = "symbol"
+    assert_equal :symbol, str.to_sym, "Expected to convert string to symbol"
+    assert_equal :symbol, str.intern, "Expected to convert string to symbol"
+  end
+
+  def test_symbol_comparison_against_string_comparison_time
+    str_time = Benchmark.measure do
+      "string" == "string"
+    end
+    sym_time = Benchmark.measure do
+      :string == :string
+    end
+    assert str_time.real > sym_time.real
+  end
 end
