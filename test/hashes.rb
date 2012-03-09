c0dfe919d5501171dd9a415933ce79492a6ee9f1
João Soares <jsoaresgeral@gmail.com>
2012-03-09 18:44:36 +0000

    refactored the code in tests from hashes.rb

--- test/hashes.rb
+++ test/hashes.rb
@@ -2,57 +2,43 @@ require 'test/unit'
 
 class TestHashes < Test::Unit::TestCase
 
-  def test_hash_creation
+  def setup
     # Note that single or double quotes, makes no difference
     # the same way as curly braces or square brackets
-    h = Hash[ :one => 1, :two => 2, :three => "three", :four => 4.0 ]
-    assert_equal({:one => 1, :two => 2, :three => 'three', :four => 4.0 }, h,
-    "Should be equal to { :one => 1, :two => 2, :three => \"three\", :four => 4.0 }")
-
-    h = Hash.new
-    h[:one] = 1; h[:two] = 2; h[:three] = 'three'; h[:four] = 4.0
-    assert_equal({:one => 1, :two => 2, :three => 'three', :four => 4.0 }, h,
-    "Should be equal to { :one => 1, :two => 2, :three => \"three\", :four => 4.0 }")
-
-    h = { :one => 1, :two => 2, :three => "three", :four => 4.0 }
-    assert_equal({:one => 1, :two => 2, :three => 'three', :four => 4.0 }, h,
-    "Should be equal to { :one => 1, :two => 2, :three => \"three\", :four => 4.0 }")
-  end
-
-  def test_syntax_changes_for_versions_later_than_1_9_x
+    @h1 = Hash[ :one => 1, :two => 2, :three => "three", :four => 4.0 ]
+    @h2 = Hash.new
+    @h2[:one] = 1; @h2[:two] = 2; @h2[:three] = 'three'; @h2[:four] = 4.0
+    @h3 = { :one => 1, :two => 2, :three => "three", :four => 4.0 }
+    
+    # Old syntax deprecated since version 1.9
+    @old_code = '{:one, 1, :two, 2 }'
+    
     # New syntax since version 1.9.x
     # Note that there may not be any space between the hash key identifier and the colon
-    code = '{ two: 2, three: 3 }'
+    @new_code = '{one: 1, two: 2 }'
     if RUBY_VERSION >= "1.9.0"
-      assert RUBY_VERSION > "1.9.0", "Version should be later than 1.9"
-      h = eval(code)
-      assert_equal({ :two => 2, :three => 3 }, h, "Should be equal to #{code}")
+      @h4 = eval(@new_code)
+    else
+      @h4 = eval(@old_code)
     end
+    
+    # Common syntax valid in both versions
+    @h5 = { :one => 1, :two => 2 }
+
+    @empty1 = Hash.new; @empty2 = {}
   end
 
-  def test_syntax_changes_for_versions_prior_to_1_9_x
-    # Old Syntax deprecated since version 1.9
-    code = '{:one, 1, :two, 2 }'
-    if RUBY_VERSION < "1.9.0"
-      assert RUBY_VERSION < "1.9.0", "Version should be prior to 1.9"
-      h = eval(code)
-      assert_equal({ :one => 1, :two => 2 }, h, "Should be equal to #{code}")
-    end
+  def test_hash_creation
+    assert_equal @h1, @h2, "Should be equal to"
+    assert_equal @h2, @h3, "Should be equal"
   end
 
-  def test_syntax_common_to_both_before_and_after_version_1_9_x
-    # Common Syntax valid in both versions
-    # Note the trailing comma doesn't break the test, it is ignored
-    h = { :one => 1, :two => 2, }
-    assert_equal({:one => 1, :two => 2 }, h, "Should be equal to { :one => 1, :two => 2 }")
+  def test_syntax_changes_introduced_in_version_1_9_x
+    assert_equal @h5, @h4, "Should represent the same hash"
   end
 
   def test_hash_equality
-    empty_h1 = Hash.new; empty_h2 = {}
-    assert_equal empty_h2, empty_h1, "Should be equal since they are both empty"
-
-    h1 = { :one => 1, :two => 2 }; h2 = { :two => 2, :one => 1 }
-    assert_equal h1, h2, "Should be equal since they contain the same content"
+    assert_equal @empty1, @empty2, "Should be equal since they are both empty"
   end
 
 end
