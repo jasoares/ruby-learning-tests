b480bd26c933d55343ba4975e341fa8082f706b7
João Soares <jsoaresgeral@gmail.com>
2012-03-12 00:04:32 +0000

    file structure organized

--- lib/ruby-learning-tests.rb
+++
@@ -1,9 +0,0 @@
-require "ruby-learning-tests/version"
-
-module Ruby
-  module Learning
-    module Tests
-      # Your code goes here...
-    end
-  end
-end
--- lib/ruby-learning-tests/version.rb
+++
@@ -1,7 +0,0 @@
-module Ruby
-  module Learning
-    module Tests
-      VERSION = "0.3.8"
-    end
-  end
-end
