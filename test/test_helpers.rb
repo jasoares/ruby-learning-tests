b65ae0babd668e6763362abaa9d0d6b36e1136a5
João Soares <jsoaresgeral@gmail.com>
2012-04-02 18:31:23 +0100

    altered the method of including the lib path to the require default paths

--- test/test_helpers.rb
+++ test/test_helpers.rb
@@ -2,4 +2,4 @@ require 'rubygems'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+$: << File.expand_path('../lib', File.dirname(__FILE__))
